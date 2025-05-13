d0a4356778e54dad291be4cf0f99dd12356b6492
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2025-05-13 08:50:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,4 +144,5 @@ def analyze():
     return jsonify({'result': reply}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)
